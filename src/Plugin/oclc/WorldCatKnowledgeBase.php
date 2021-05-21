9cd9b11196bf482e87f900939aa779c70de0bc36
Brian Cassidy <brian.cassidy@unb.ca>
2021-05-21 12:36:50 -0300

    update KB annotation to new url+query format

--- src/Plugin/oclc/WorldCatKnowledgeBase.php
+++ src/Plugin/oclc/WorldCatKnowledgeBase.php
@@ -12,13 +12,69 @@ use Psr\Http\Message\ResponseInterface;
  *   id = "worldcat_knowledge_base",
  *   label = @Translation("WorldCat Knowledge Base API"),
  *   endpoints = {
- *     "search-collections" = "https://worldcat.org/webservices/kb/rest/collections/search?alt=json&q={@query}&title={@title}&provider_uid={@provider_uid}&collection_uid={@collection_uid}&search-type={@search-type}&startIndex={@startIndex}&itemsPerPage={@itemsPerPage}&orderBy={@orderBy}",
- *     "read-collection" = "https://worldcat.org/webservices/kb/rest/collections/{@collection_uid}?alt=json",
- *     "search-entries" = "https://worldcat.org/webservices/kb/rest/entries/search?alt=json&q={@query}&title={@title}&provider_uid={@provider_uid}&collection_uid={@collection_uid}&issn={@issn}&isbn={@isbn}&oclcnum={@oclcnum}&content={@content}&search-type={@search-type}&startIndex={@startIndex}&itemsPerPage={@itemsPerPage}&orderBy={@orderBy}",
- *     "read-entry" = "https://worldcat.org/webservices/kb/rest/entries/{@entry_uid}?alt=json",
- *     "search-providers" = "https://worldcat.org/webservices/kb/rest/providers/search?alt=json&q={@query}&title={@title}&provider_uid={@provider_uid}&startIndex={@startIndex}&itemsPerPage={@itemsPerPage}&orderBy={@orderBy}",
- *     "read-provider" = "https://worldcat.org/webservices/kb/rest/providers/{@provider_uid}?alt=json",
- *   }
+ *     "search-collections" = {
+ *       "url" = "https://worldcat.org/webservices/kb/rest/collections/search",
+ *       "query" = {
+ *         "alt" = "json",
+ *         "q" = "",
+ *         "title" = "",
+ *         "provider_uid" = "",
+ *         "collection_uid" = "",
+ *         "search-type" = "",
+ *         "startIndex" = "1",
+ *         "itemsPerPage" = "25",
+ *         "orderBy" = "",
+ *       },
+ *     },
+ *     "read-collection" = {
+ *       "url" = "https://worldcat.org/webservices/kb/rest/collections/{@collection_uid}",
+ *       "query" = {
+ *         "alt"= "json",
+ *       },
+ *     },
+ *     "search-entries" = {
+ *       "url" = "https://worldcat.org/webservices/kb/rest/entries/search",
+ *       "query" = {
+ *         "alt" = "json",
+ *         "q" = "",
+ *         "title" = "",
+ *         "provider_uid" = "",
+ *         "collection_uid" = "",
+ *         "issn" = "",
+ *         "isbn" = "",
+ *         "oclcnum" = "",
+ *         "content" = "",
+ *         "search-type" = "",
+ *         "startIndex" = "1",
+ *         "itemsPerPage" = "25",
+ *         "orderBy" = "",
+ *       },
+ *     },
+ *     "read-entry" = {
+ *       "url" = "https://worldcat.org/webservices/kb/rest/entries/{@entry_uid}",
+ *       "query" = {
+ *         "alt"= "json",
+ *       },
+ *     },
+ *     "search-providers" = {
+ *       "url" = "https://worldcat.org/webservices/kb/rest/providers/search",
+ *       "query" = {
+ *         "alt" = "json",
+ *         "q" = "",
+ *         "title" = "",
+ *         "provider_uid" = "",
+ *         "startIndex" = "1",
+ *         "itemsPerPage" = "25",
+ *         "orderBy" = "",
+ *       },
+ *     },
+ *     "read-provider" = {
+ *       "url" = "https://worldcat.org/webservices/kb/rest/providers/{@provider_uid}",
+ *       "query" = {
+ *         "alt"= "json",
+ *       },
+ *     },
+ *   },
  * )
  *
  * @link https://platform.worldcat.org/api-explorer/apis/kbwcapi
