f4c7282db70af921b54c7b6812121ecf485e0980
Brian Cassidy <brian.cassidy@unb.ca>
2025-01-22 13:05:17 -0400

    add worldcat metadata api plugin

---
+++ src/Plugin/oclc/WorldCatMetadata.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Drupal\oclc_api\Plugin\oclc;
+
+use Drupal\oclc_api\Annotation\OclcApi;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Plugin to interact with the WorldCat Metadata API.
+ *
+ * @OclcApi(
+ *   id = "worldcat_metadata",
+ *   label = @Translation("WorldCat Metadata API"),
+ *   endpoints = {
+ *     "get-bibs-summary-holdings" = {
+ *       "url" = "https://metadata.api.oclc.org/worldcat/search/bibs-summary-holdings",
+ *       "query" = {
+ *         "oclcNumber" = "",
+ *         "heldInCountry" = "",
+ *       },
+ *     },
+ *   },
+ *   scopes = {
+ *     "WorldCatMetadataAPI:view_summary_holdings",
+ *   }
+ * )
+ *
+ * @link https://developer.api.oclc.org/wc-metadata-v2
+ *
+ * @package Drupal\oclc_api\Plugin\oclc
+ */
+class WorldCatMetadata extends OclcApiBase {
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function buildHeaders() {
+    $token = $this->oclcAuthorizer()
+      ->getToken();
+    return parent::buildHeaders() + [
+      'Authorization' => "Bearer {$token}",
+    ];
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function buildUrlQuery(array $endpoint, array $params) {
+    if (empty($params['heldInCountry'])) {
+      unset($endpoint['query']['heldInCountry']);
+      unset($params['heldInCountry']);
+    }
+    return parent::buildUrlQuery($endpoint, $params);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  protected function buildResponse(ResponseInterface $response) {
+    return json_decode($response->getBody()->getContents());
+  }
+
+}
